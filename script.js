d4562ccde1d4331cd473787cf1c93fac04bd0497
nehachandra22 <nehachandra800@gmail.com>
2024-01-15 20:29:19 +0530

    created navbar

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+const sidebar = document.querySelector('.sidebar');
+const hidehamburger = document.querySelector('#hamburger');
+const hidecross = document.querySelector('#cross');
+console.log(sidebar);
+
+function showSidebar(){
+    sidebar.style.display='block';
+    hidecross.style.display='block';
+    hidehamburger.style.display='none';
+}
+function hideSidebar(){
+    sidebar.style.display='none';
+    hidehamburger.style.display="block";
+    hidecross.style.display='none';
+}
