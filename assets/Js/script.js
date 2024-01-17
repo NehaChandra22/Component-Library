e58926e5627e74ed7f7f69e22c155f3317567222
nehachandra22 <nehachandra800@gmail.com>
2024-01-17 20:52:17 +0530

    feat aside

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,24 +1,39 @@
 const sidebar = document.querySelector('.left-sidebar');
-const hidehamburger = document.querySelector('#hamburger');
-const hidecross = document.querySelector('#cross-icon');
+const hidehamburgerL = document.querySelector('#hamburger-L');
+const hidehamburgerR = document.querySelector('#hamburger-R');
+const hidecrossL = document.querySelector('#cross-icon-L');
+const hidecrossR = document.querySelector('#cross-icon-R');
 const navbar = document.querySelector('.navbar');
 const Darkmode = document.querySelector('.dark-mode');
 const Lightmode = document.querySelector('.light-mode');
 const body = document.querySelector('body');
 const linkcont = document.querySelector('.link-container');
-
+const aside = document.querySelector('aside')
 
 function showSidebar() {
     sidebar.style.display = 'block';
-    hidecross.style.display = 'block';
-    hidehamburger.style.display = 'none';
+    hidecrossL.style.display = 'block';
+    hidehamburgerL.style.display = 'none';
+    
 }
 function hideSidebar() {
     sidebar.style.display = 'none';
-    hidehamburger.style.display = "block";
-    hidecross.style.display = 'none';
+    hidehamburgerL.style.display = "block";
+    hidecrossL.style.display = 'none';
 }
-
+ 
+function showAside() {
+    aside.style.display = 'block';
+    hidecrossR.style.display = 'block';
+    hidehamburgerR.style.display = 'none';
+    
+}
+function hideAside() {
+    aside.style.display = 'none';
+    hidehamburgerR.style.display = "block";
+    hidecrossR.style.display = 'none';
+}
+ 
 // dark-mode 
 function darkmode() {
     Lightmode.style.display = 'block';
